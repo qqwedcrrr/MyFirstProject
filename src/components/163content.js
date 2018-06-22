a2f8903868f289c14107768b6114b97e947f877c
ZitaoYang <ziyang@merkleinc.com>
2018-06-22 18:22:03 +0800

    viewpage update
    viewpage update

--- src/components/163content.js
+++ src/components/163content.js
@@ -51,12 +51,24 @@ class Viewpage extends Component{
 			bgcolor:viewPage[0].bgcolor,
 			fadeInOut:fadeOver,
 			flag:3
-		}
+		};
+		this.handlePointClick = this.handlePointClick.bind(this)
 	};
-	componentDidMount() {
-			this.timeId = setInterval(
-			i=>this.showfade(), 1000)
+	
+	handlePointClick(id){
+		i = id;
+		this.setState({
+			fadeInOut:fadeIn,
+			viewpageimg:viewPage[id-1].src,
+			bgcolor:viewPage[id-1].bgcolor,
+			flag:3
+		})
+	}
 
+	componentDidMount() {
+		this.timeId = setInterval(
+		i=>this.showfade(), 1000)
+	
   	}
     componentWillUnmount() {
     	clearInterval(this.timeId)
@@ -71,7 +83,13 @@ class Viewpage extends Component{
 	    		this.fadeout();
 	    		break;
 	    	case 3:
-	    		this.tick()
+	    		this.tick().then(
+	    			clearInterval(this.timeId)).then(
+						setTimeout(()=>{
+							this.timeId = setInterval(
+							i=>this.showfade(), 1000)
+						}, 3000)
+	    		);
 	    		break;
 		}
   	}
@@ -92,11 +110,15 @@ class Viewpage extends Component{
 	}
 
     tick() {
-		this.setState({
+		return new Promise((resolve,reject)=>{
+			this.setState({
 			fadeInOut:fadeOver,
 			flag:2
+			})
 		})
     }
+
+
 
 	render(){
 		return(
@@ -105,7 +127,16 @@ class Viewpage extends Component{
 					<div className="VPleft">
 						<a className="logoAtag" hidefocus="true" href="/#">
 						<img src={this.state.viewpageimg} style={this.state.fadeInOut} width="730" height="336" />
-						</a>	
+						</a>
+						<a hidefocus="true" href="javasript:void(0)" className="viewPageL">&lt;</a>	
+						<a hidefocus="true" href="javasript:void(0)" className="viewPageR">&gt;</a>
+						<div className="viewpagePointlist">
+						{
+							viewPage.map(item=>(
+								<ViewpagePoint key={item.id} pointid={i} dataIndex={item.id} onClick={e=>{this.handlePointClick(item.id)}}  />
+							))
+						}
+						</div>	
 					</div>
 					<div className="VPright">
 						<a className="downloadtag" hidefocus="true" href="/#">
@@ -120,6 +151,10 @@ class Viewpage extends Component{
 		)
 	}
 }
+
+const ViewpagePoint = ({onClick, pointid, dataIndex}) => (
+	<a hidefocus="true" href="javasript:void(0)" className="viewpagePoint" onClick={onClick} style={{ backgroundPosition: dataIndex == pointid ? '-16px -343px' : '3px -343px'}}></a>
+)
 
 class Lcontent extends Component{	
 	constructor(props){
