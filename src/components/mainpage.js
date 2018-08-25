8f59d4cd825a94f28a82b816e4f5fee08f4f5f13
yzt <493105923@qq.com>
2018-08-25 10:53:12 +0800

    new update

--- src/components/mainpage.js
+++ src/components/mainpage.js
@@ -2,15 +2,27 @@ import React from 'react'
 import {NavLink as Link } from 'react-router-dom';
 
 
+const liButton = {
+  background: '#8aa',
+  borderRadius:'5px',
+  display:'block',
+  textAlign:'center',
+  padding:'8px',
+  fontSize:'16px',
+  color:'#fff',
+  width:'100%',
+  textWrap:'none'
+}
+
 const Header = () => (
   <header>
     <nav>
       <ul>
-        <li><Link to='/'>Home Page</Link></li>
-        <li><Link to='/roster'>Music163 demo</Link></li>
-        <li><Link to='/schedule'>On Hold</Link></li>
-        <li><Link to='/Counter'>Counter</Link></li>
-        <li><Link to='/Todo'>TodoList</Link></li>
+        <li><Link style={liButton} to='/'>Home Page</Link></li>
+        <li><Link style={liButton} to='/roster'>Music163&nbsp;demo</Link></li>
+        <li><Link style={liButton} to='/schedule'>On Hold</Link></li>
+        <li><Link style={liButton} to='/Counter'>Counter</Link></li>
+        <li><Link style={liButton} to='/Todo'>TodoList</Link></li>
       </ul>
     </nav>
   </header>
